fc5999efeeb14ccab79a60ea1a57fc54c564fb4e
nasib <mnnasib7777@gmail.com>
2022-03-29 02:48:02 +0500

    intial_commit

---
+++ app/Http/Controllers/PdfController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Http\Request;
+
+class PdfController extends Controller
+{
+    public function getPDF(Request $request)
+    {
+
+        $pdf = Pdf::loadView('pdf.certificate', ['data', 'data']);
+        return $pdf->download('certificate.pdf');
+    }
+
+    public function pdfView(Request $request)
+    {
+
+        return view('pdf.certificate');
+    }
+}
---
+++ resources/views/pdf/certificate.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+</head>
+<body>
+
+<img alt="" style="max-width: 100%" src="{{ public_path("/images/pdf/main-heading.jpeg") }}"/>
+<h1 style="text-align: center">Zertifikat</h1>
+<h2>deutsch A1</h2>
+<form action="formsubmit.php" method="post">
+    <b>Input Text</b>
+    <input type="text" size="90" name="inputfield" value="" title="The title attribute works like a tool-tip" />
+</form>
+</body>
+</html>
