52b5ca64aa0a254a0942fe8449e9328e9406f094
nasib <mnnasib7777@gmail.com>
2022-05-12 00:48:08 +0500

    minor changes

--- resources/js/module/students/certificate/generate.vue
+++ resources/js/module/students/certificate/generate.vue
@@ -20,8 +20,8 @@
                         v-for="certificate in types"
                         :key="certificate.id"
                         :data-hasLanguageModule="certificate.has_module"
-                        >{{ certificate.name }}</a-select-option
-                    >
+                        >{{ certificate.name }}
+                    </a-select-option>
                 </a-select>
             </a-form-item>
             <a-form-item label="Listening">
